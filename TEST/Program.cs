dcaa4d8398e06bab377a4f9fa86aafad672bcab4
Atakan Süslü <serseri_madrid@hotmail.com>
2019-04-30 20:26:50 +0300

    Test fonksiyonları oluşturuldu.

--- TEST/Program.cs
+++ TEST/Program.cs
@@ -12,13 +12,19 @@ namespace TEST
     {
         static void Main(string[] args)
         {
-            ExcelBL bl = new ExcelBL("TEST.xls",IslemTipi.OKUMA);
-            var test = bl.Hucre("Sayfa1","A1:A3");
-            bl.Kapat();
-
+            SQLTEST();
+        }
+        static void SQLTEST()
+        {
             var con = new SqlConnection();
             con.Insert(new Models.sbptTest() { ID = 1, Isim = "atakan" });
             var k = new List<string>();
+        }
+        static void ExcelHucreCek()
+        {
+            ExcelBL bl = new ExcelBL("TEST.xls", IslemTipi.OKUMA);
+            var test = bl.Hucre("Sayfa1", "A1:A3");
+            bl.Kapat();
             //com.Degisken<ExcelBL>(ref k);
             //bl.EkleBaslik(bl.GetType(),"Sayfa1");
         }
