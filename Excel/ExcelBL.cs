5d2c0bddb089b83187559e17bd2132c1f86fa197
Atakan Süslü <atakansuslu@gmail.com>
2021-02-28 02:43:07 +0300

    Connection oluşturma metodu eklendi. Tablo oluşturma metodu eklendi. Dosya silme metodu eklendi. Çoklu kayıt ekleme metodu "Ekle" metodundan çalışır hale getirildi. Başlık ekleme metodunda yazım iyileştirmeleri(çalışmıyor).

--- Excel/ExcelBL.cs
+++ Excel/ExcelBL.cs
@@ -1,11 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.OleDb;
-using System.Data.SqlClient;
 using System.Linq;
 using System.IO;
-using System.Text;
-using System.Threading.Tasks;
 using Modelleyici;
 using System.Reflection;
 
@@ -17,7 +14,7 @@ namespace Excel
     }
     public class ExcelBL
     {
-        OleDbCommand com = new OleDbCommand();
+        OleDbCommand com;
         OleDbConnection con;
         public IslemTipi IslemTipi { get; set; }
         public string DosyaYolu { get; set; }
@@ -25,6 +22,11 @@ namespace Excel
         {
             this.DosyaYolu = DosyaYolu;
             this.IslemTipi = IslemTipi;
+            ConnectionOlustur();
+            com = con.CreateCommand();
+        }
+        private void ConnectionOlustur()
+        {
             var Ozellikler = "";
             switch (IslemTipi)
             {
@@ -33,15 +35,72 @@ namespace Excel
                     //Ozellikler = "Excel 12.0;HDR=NO;";
                     break;
                 case IslemTipi.YAZMA:
-                    Ozellikler = "Excel 12.0;HDR=YES;";
-                    if (!File.Exists(DosyaYolu))
-                        File.Create(DosyaYolu);
+                    Ozellikler = "Excel 12.0 Xml;HDR=YES;";
                     break;
             }
-
             con = new OleDbConnection($"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={DosyaYolu};Extended Properties=\"{Ozellikler}\"");
             con.Open();
-            com.Connection = con;
+        }
+        public void TabloOlustur<T>(T Object, bool DosyavarsaSil = false)
+        {
+            TabloOlustur(typeof(T), DosyavarsaSil: DosyavarsaSil);
+            Ekle(Object);
+        }
+        public void TabloOlustur<T>(List<T> Object, bool DosyavarsaSil = false)
+        {
+            TabloOlustur(typeof(T), DosyavarsaSil: DosyavarsaSil);
+            Ekle(Object);
+        }
+        public void TabloOlustur(Type _Type, bool DosyavarsaSil = false)
+        {
+            if (DosyavarsaSil)
+            {
+                DosyaSil();
+                ConnectionOlustur();
+                com = con.CreateCommand();
+            }
+            var TabloIsmi = _Type.Name;
+            com.CommandText = string.Format("create table {0} (@@)", TabloIsmi);
+
+            var Sutunlar = new List<string>();
+            foreach (var prop in _Type.GetProperties())
+            {
+                var Type = prop.PropertyType;
+                var Isim = prop.Name;
+                var Sutun = string.Format("{0} {1}", Isim, GetExcelDataType(Type));
+                Sutunlar.Add(Sutun);
+            }
+            com.CommandText = com.CommandText.Replace("@@", String.Join(",", Sutunlar));
+            com.ExecuteNonQuery();
+
+        }
+        private void TabloSil(Type _Type, bool BaslikSil = false)
+        {
+            var TabloIsmi = _Type.Name;
+            var Sutunlar = new List<string>();
+            foreach (var prop in _Type.GetProperties())
+                Sutunlar.Add(string.Format("{0}=''", prop.Name));
+            com.CommandText = $"update [{TabloIsmi}$] set {string.Join(",", Sutunlar)}";
+            com.ExecuteNonQuery();
+            com.Parameters.Clear();
+            if (BaslikSil)
+            {
+                com.CommandText = $"drop table [{TabloIsmi}$]";
+                com.ExecuteNonQuery();
+            }
+        }
+        public void DosyaSil()
+        {
+            Kapat();
+            File.Delete(DosyaYolu);
+        }
+        private void TabloSil<T>(T Object, bool BaslikSil = false)
+        {
+            TabloSil(typeof(T), BaslikSil: BaslikSil);
+        }
+        private void TabloSil<T>(List<T> Object, bool BaslikSil = false)
+        {
+            TabloSil(typeof(T), BaslikSil: BaslikSil);
         }
         public List<Dictionary<string, dynamic>> Tablo(string TabloIsmi, string Range = "", bool Trim = true, bool WhiteSpace = false)
         {
@@ -98,13 +157,13 @@ namespace Excel
         /// <param name="Range">Çekilmek istenen kayıtlar hangi sutunlar arasında. (A1:F3)</param>
         /// <param name="Trim">Boşluklar silinsin mi</param>
         /// <returns></returns>
-        public List<T> Tablo<T>(string TabloIsmi = "", string Range = "",bool ConvertType=true)
+        public List<T> Tablo<T>(string TabloIsmi = "", string Range = "", bool ConvertType = true)
         {
             if (string.IsNullOrEmpty(TabloIsmi))
                 TabloIsmi = typeof(T).Name;
             com.CommandText = $"select * from [{TabloIsmi}${Range}]";
             var y = new List<string>();
-            var Sonuc = com.Liste<T>(ref y,ConvertType:ConvertType);
+            var Sonuc = com.Liste<T>(ref y, ConvertType: ConvertType);
             for (int i = 0; i < Sonuc.Count; i++)
             {
                 int j = 0;
@@ -114,9 +173,9 @@ namespace Excel
                     var val = item.GetValue(Sonuc[i]);
                     var val2 = item.GetValue(Activator.CreateInstance<T>());
                     if (object.Equals(val, val2))
-                        j++;   
+                        j++;
                 }
-                if (j==propCount)
+                if (j == propCount)
                 {
                     Sonuc.RemoveRange(i, Sonuc.Count - i);
                     return Sonuc;
@@ -239,23 +298,23 @@ namespace Excel
         }
         /// <summary>
         /// İstenen hücreyi günceller
+        /// HDR NO
         /// </summary>
         /// <param name="TabloIsmi">Guncellenmek istenen tablo ismi.</param>
         /// <param name="Range">Guncellenmek istenen hucrenin sutunu. (A1:A1)</param>
         /// <param name="Deger">Yeni değer</param>
         public void GuncelleHucre(string TabloIsmi, string Range, string Deger)
         {
-            //HDR NO
             com.CommandText = $"update [{TabloIsmi}${Range}] set F1='{Deger}'";
             com.ExecuteNonQuery();
         }
         /// <summary>
         /// İstenen hücreyi günceller
+        /// HDR YES
         /// </summary>
         /// <param name="sql"></param>
         public void GuncelleHucre(string sql)
         {
-            //HDR YES
             com.CommandText = sql;
             com.ExecuteNonQuery();
         }
@@ -270,21 +329,7 @@ namespace Excel
             //EkleBaslik(Tablo.FirstOrDefault().GetType(), TabloIsmi);
             if (TabloIsmi.Equals(""))
                 TabloIsmi = Tablo.FirstOrDefault().GetType().Name;
-            foreach (var Kayit in Tablo)
-            {
-                var Alanlar = "";
-                var Degerler = "";
-                foreach (var prop in Kayit.GetType().GetProperties())
-                {
-                    Alanlar += prop.Name + ",";
-                    Degerler += "\'" + prop.GetValue(Kayit).ToString() + "\',";
-                }
-                Alanlar = Alanlar.Substring(0, Alanlar.Length - 1);
-                Degerler = Degerler.Substring(0, Degerler.Length - 1);
-                com.CommandText = $"insert into [{TabloIsmi}$] ({Alanlar}) values ({Degerler})";
-                com.ExecuteNonQuery();
-            }
-
+            Tablo.ForEach(x => Ekle(x));
         }
         /// <summary>
         /// Verilen modelin alanlarına göre excele kayıt atar.
@@ -299,51 +344,45 @@ namespace Excel
 
             var Alanlar = "";
             var Degerler = "";
+
             foreach (var prop in Kayit.GetType().GetProperties())
             {
+                var oDeger = prop.GetValue(Kayit);
+                var Deger = "";
+                if (oDeger != null)
+                    Deger = oDeger.ToString();
                 Alanlar += prop.Name + ",";
-                Degerler += "'" + prop.GetValue(Kayit).ToString() + "',";
+                com.Parameters.AddWithValue(string.Format("@{0}", prop.Name), Deger);
+                Degerler += "?,";
+                //Degerler += "\'" + prop.GetValue(Kayit).ToString() + "\',";
             }
             Alanlar = Alanlar.Substring(0, Alanlar.Length - 1);
             Degerler = Degerler.Substring(0, Degerler.Length - 1);
             com.CommandText = $"insert into [{TabloIsmi}$] ({Alanlar}) values ({Degerler})";
             com.ExecuteNonQuery();
+            com.Parameters.Clear();
         }
         /// <summary>
         /// Excel Dosyasına Başlık Girişi Yap. Eklenmek istenen modelin alanları ilk satır olarak girilir. 
         /// </summary>
         /// <param name="Kayit">Eklenicek kayıtların tipi.</param>
         /// <param name="TabloIsmi">Kayıtların eklenmek istendiği tablo ismi. Boş geçilirse modelin adı yazılır.</param>
-        public void EkleBaslik(Type Kayit, string TabloIsmi = "")
+        public void EkleBaslik<T>(T Kayit, string TabloIsmi = "")
         {
-
-            var _con = con.ConnectionString;
-            con.Close();
-            var Ozellikler = "Excel 12.0;HDR=NO;";
-            con.ConnectionString = $"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={DosyaYolu};Extended Properties=\"{Ozellikler}\"";
-            con.Open();
-            com.Connection = con;
-
             if (TabloIsmi.Equals(""))
                 TabloIsmi = Kayit.GetType().Name;
-
-            var Alanlar = "";
-            var Degerler = "";
             int i = 1;
-            foreach (var prop in Kayit.GetProperties())
+            var Alanlar = new List<string>();
+            var Degerler = new List<string>();
+            foreach (var prop in typeof(T).GetProperties())
             {
-                Alanlar += $"[F{i++}],";
-                Degerler += $"'{ prop.Name}',";
+                Alanlar.Add($"[F{i++}]");
+                Degerler.Add($"?");
+                com.Parameters.AddWithValue($"@{prop.Name}", prop.Name);
             }
-            Alanlar = Alanlar.Substring(0, Alanlar.Length - 1);
-            Degerler = Degerler.Substring(0, Degerler.Length - 1);
-            com.CommandText = $"insert into [{TabloIsmi}$] (F1,F2,F3)values(1,2,3)";
-            com.ExecuteNonQuery();
-            con.Close();
-
-            con.ConnectionString = _con;
-            con.Open();
-            com.Connection = con;
+            com.CommandText = $"insert into [{TabloIsmi}$] ({string.Join(",", Alanlar)})values({string.Join(",", Degerler)})";
+            var rr = com.ExecuteNonQuery();
+            com.Parameters.Clear();
         }
         /// <summary>
         /// Tüm nesneleri serbest bırak
@@ -353,6 +392,50 @@ namespace Excel
             com.Dispose();
             con.Close();
             con.Dispose();
+        }
+
+        private string GetExcelDataType(Type type)
+        {
+            var name = type.Name;
+            if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>))
+            {
+                name = type.GetGenericArguments()[0].Name;
+            }
+
+            switch (name)
+            {
+                case "Guid":
+                    return "text";
+                case "Boolean":
+                    return "number";
+                case "Byte":
+                    return "number";
+                case "Int16":
+                    return "number";
+                case "Int32":
+                    return "number";
+                case "Int64":
+                    return "number";
+                case "Decimal":
+                    return "number";
+                case "Single":
+                    return "number";
+                case "Double":
+                    return "number";
+                case "DateTime":
+                    return "datetime";
+                case "String":
+                case "Char[]":
+                    return "text";
+                case "Char":
+                    return "text";
+                case "Byte[]":
+                    return "text";
+                case "Object":
+                    return "text";
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
         }
     }
 }
